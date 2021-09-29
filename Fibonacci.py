502141c476badc1fd3a16104423a0ed09f94cd2c
Ursii12 <90208661+Ursii12@users.noreply.github.com>
2021-09-29 23:55:29 +0300

    Fizzbuzz

---
+++ Fibonacci.py
@@ -0,0 +1,19 @@
+numar_termeni = int(input("Numar termeni"))
+n1=0
+n2=1
+count = 0
+
+if numar_termeni <= 0:
+    print ("Introduceti un numar pozitiv")
+else:
+    if numar_termeni ==  :
+        print("Secventa Fibonacci" , numar_termeni, ":")
+        print(n1)
+    else:
+      print("Secventa Fibonacci:")
+      while count<numar_termeni:
+          print(n1)
+          x=n1+n2
+          n1=n2
+          n2=x
+          count +=1
